a156a60bb764358ca66d9e940825250590d3d455
KasperGoes <kaspergoes@gmail.com>
2022-06-09 18:22:08 +0200

    test

--- communityDetection/communityDetection/Program.cs
+++ communityDetection/communityDetection/Program.cs
@@ -12,13 +12,13 @@ namespace communityDetection
             string filename2 = "../../../graphs/kleine graaf partition.txt";
             StreamReader readeredges = new StreamReader(filename1);
             StreamReader readerpartition = new StreamReader(filename2);
-
+            Console.WriteLine(readeredges);
             Graph graph = readGraphs.create_graph(readeredges, readerpartition);
             //set_initial_communities(graph);
 
-            Louvain.louvain(graph);
+            //Louvain.louvain(graph);
             //test_modularity(graph);
-            //Graph result = SA.simulatedAnnealing(graph);
+            float Result = SA.simulatedAnnealing(graph);
         }
 
         static void set_initial_communities(Graph graph)
