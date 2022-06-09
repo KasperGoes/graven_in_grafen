39cceba481d332bdf654baa0b509e3dbecabfc79
KasperGoes <kaspergoes@gmail.com>
2022-06-09 23:15:16 +0200

    random neighbour pakken, delta schalen

--- communityDetection/communityDetection/simulatedAnnealing.cs
+++ communityDetection/communityDetection/simulatedAnnealing.cs
@@ -7,13 +7,14 @@ namespace communityDetection
 
 		public static float simulatedAnnealing(Graph g)
 		{
+            Console.WriteLine(Modularity.modularity(g));
 			int iteration =-1;
 
-			float alpha =0.999F;
+			float alpha =0.90F;
 
 			//vm veel te hoge temp met onze delta
-			double temperature = 400.0;
-			double epsilon = 0.001;
+			double temperature = 1.00;
+			double epsilon = 0.1;
 			
 			while(temperature > epsilon)
 			{
@@ -32,6 +33,7 @@ namespace communityDetection
 				// if we dont improve we accept the change with a chance
 				else
 				{
+					delta = delta * 2*g.m;
 					double acceptProb = Math.Exp(delta/temperature);
 					double random_probability = random.NextDouble();
 					if (random_probability < Math.Exp(delta/temperature))
@@ -42,14 +44,15 @@ namespace communityDetection
 				}
 
 				// we cool every iteration
-				temperature = temperature*alpha;	
+				temperature = temperature*alpha;
 
 				//print modularity every 400 iterations
-				if (iteration%400==0)
-
-				Console.WriteLine(Modularity.modularity(g));
+				if (iteration % 400 == 0)
+				{
+					Console.WriteLine(Modularity.modularity(g));
+				}
 			}
-
+            Console.WriteLine(Modularity.modularity(g));
 			return Modularity.modularity(g);
 		}
 
@@ -68,29 +71,37 @@ namespace communityDetection
 
 				// Look at the neightbours of vertex and see if they have a different community
 				// TO DO: Random neighbour instead of first neighbour in the list
-				int index = 0;
-				int neighbour = v.neighbours[index];
-				u = g.vertices[neighbour];
-				bool found_candidate_neighbour = true;
+				
 
+				
+				int random_neighbour_index = random.Next(v.neighbours.Count);
+				u = g.vertices[v.neighbours[random_neighbour_index]];
+				//int index = 0;
+				//int neighbour = v.neighbours[index];
+				//u = g.vertices[neighbour];
+				//bool found_candidate_neighbour = true;
 
+				if (u.community != v.community)
+				{ return (g.communities[u.community], v); }
+             
 				// While neighbour u has the same community as v, try a new neighbour 
-				while (u.community == v.community)
-				{
-					index++;
+				//while (u.community == v.community)
+				//{
+				//	index++;
 
-					if (index >= v.neighbours.Count)
-                    {
-						found_candidate_neighbour = false;
-						break;
-                    }
+				//	if (index >= v.neighbours.count)
+    //                {
+				//		found_candidate_neighbour = false;
+				//		break;
+    //                }
 
-					neighbour = v.neighbours[index];
-					u = g.vertices[neighbour];
-				}
+				//	neighbour = v.neighbours[index];
+				//	u = g.vertices[neighbour];
+				//}
 
-				if(found_candidate_neighbour)
-					return (g.communities[u.community], v);
+				//if(found_candidate_neighbour)
+
+				//	return (g.communities[u.community], v);
 			}
 
 			return (null, null);
