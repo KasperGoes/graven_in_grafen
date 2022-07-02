a19d992768478e15206504f6286fa178fd5f8975
KasperGoes <kaspergoes@gmail.com>
2022-07-03 00:37:56 +0200

    mdularity added

--- graphGeneration/nmi.py
+++ graphGeneration/nmi.py
@@ -3,20 +3,22 @@ import community as community_louvain
 import networkit as nk
 from datetime import datetime
 
-def partitionparse(partition, withtime):
-    time = 0
+def partitionparse(partition, withtime_and_modularity):
+    time, modularity = 0
     with open(partition) as f:
         input = f.read().split("\n")
 
         if input[-1] == "":
             input.pop()
 
-        if withtime:
+        if withtime_and_modularity:
             time = input[0]
             input = input [1:]
 
+            modularity = 0
+
     input = [int(i) for i in input]
-    return input, time
+    return input, time, modularity
 
 def NMI_calc(partition1, partition2):
 
@@ -25,14 +27,17 @@ def NMI_calc(partition1, partition2):
     return NMI
     
 def maincalc(partition1, partition2):
-    part1, time = partitionparse(partition1, 0)
-    part2, time = partitionparse(partition2, 1)
+    part1, time, modularity = partitionparse(partition1, 0)
+    part2, time, modularity = partitionparse(partition2, 1)
     # part3 = [y for x,y in community_louvain.best_partition(graph).items()]
     # normalized_mutual_info_score(part3, partition2)
     NMI = NMI_calc(part1, part2)
 
-    calculatation_time = datetime.strptime(time,"%H:%M:%S.%f").second
-    return NMI, calculatation_time
+    calculatation_time = datetime.strptime(time,"%H:%M:%S.%f")
+    seconds =('"%H:%M:%S.%f"'%(calculatation_time.minute,calculatation_time.second,calculatation_time.microsecond))[:-4]
+
+    
+    return NMI, seconds, modularity
 
 
 if __name__ == "__main__":
--- graphGeneration/results.py
+++ graphGeneration/results.py
@@ -59,26 +59,29 @@ def calculateNMI(Experiment, Answer):
 
     for mu_exp, mu_ans,  mu in zip(Experiment, Answer, mulist):
 
-        Results_Hybrid_time_per_mu = []
-        Results_Hybrid_NMI_per_mu = []
+        Results_Experiment_time_per_mu = []
+        Results_Experiment_NMI_per_mu = []
+        Results_Experiment_modularity_per_mu = []
 
         for graph_ans, graph_exp in zip(mu_ans, mu_exp):
 
-            NMI, calculation_time = maincalc(graph_ans, graph_exp)
+            NMI, calculation_time, modularity = maincalc(graph_ans, graph_exp)
 
-            Results_Hybrid_NMI_per_mu.append(NMI)
-            Results_Hybrid_time_per_mu.append(calculation_time)
+            Results_Experiment_NMI_per_mu.append(NMI)
+            Results_Experiment_time_per_mu.append(calculation_time)
+            Results_Experiment_modularity_per_mu.append(modularity)
 
-        Result_Experiment[mu] = confidence_interval_and_average_time(Results_Hybrid_NMI_per_mu, Results_Hybrid_time_per_mu)
-
+        Result_Experiment[mu] = confidence_interval_and_average_time(Results_Experiment_NMI_per_mu, Results_Experiment_time_per_mu, Results_Experiment_modularity_per_mu)
 
     return Result_Experiment
 
-def confidence_interval_and_average_time(Results_Hybrid_NMI_per_mu, Results_Hybrid_time_per_mu):
+def confidence_interval_and_average_time(Results_Hybrid_NMI_per_mu, Results_Experiment_time_per_mu, Results_Experiment_modularity_per_mu):
 
-    averagetime = statistics.mean(Results_Hybrid_time_per_mu)
+    averagetime = statistics.mean(Results_Experiment_time_per_mu)
+    averagemod = statistics.mean(Results_Experiment_modularity_per_mu)
     mean, lowconf, highconf = mean_confidence_interval(Results_Hybrid_NMI_per_mu)
-    Answer = {"mean_NMI": round(mean,2), "Lowerbound": round(lowconf,2), "Upperbound": round(highconf,2), "average_time": averagetime}
+
+    Answer = {"mean_NMI": round(mean,3), "Confidence": (round(lowconf,3),round(highconf,3)), "mean Modularity": round(averagemod,3), "average_time": round(averagetime,3)}
 
     return Answer
 
@@ -91,13 +94,32 @@ def mean_confidence_interval(data, confidence=0.95):
     h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
     return m, m-h, m+h
 
-def writing_csv(name, data):
-    with open(f'Results/{name}.csv', 'w') as f:
-        w = csv.DictWriter(f, data.keys())
-        w.writeheader()
-        w.writerow(data)
-    
+def printing_results(name, Resultstotal):
 
+    for dictionary in Resultstotal:
+        for key, value in dictionary.items():
+
+            print(f"{dictionary}:")
+            print()
+
+            print(key, value)
+            print("------------------------------------")
+
+def writing_txt(data):
+
+    for dictionary in data:
+
+        with open("myfile.txt", 'w') as f: 
+            for key, value in dictionary.items(): 
+                f.write('%s:%s\n' % (key, value))
+
+    # for result in data:
+        
+    #     with open(f'Results/{result}.csv', 'w') as f:
+    #         w = csv.DictWriter(f, data.keys())
+    #         w.writeheader()
+    #         w.writerow(data)
+    
 
 def main():
 
@@ -105,28 +127,36 @@ def main():
     #graph sizes used in the experiment
     graphsizes = ["10000","20000","50000"]
 
-    path1000 = "HybirdLouvainSA/HybirdLouvainSA/partitions/10000"
-    path10000 = "HybirdLouvainSA/HybirdLouvainSA/partitions/20000"
+    path10000 = "HybirdLouvainSA/HybirdLouvainSA/partitions/10000"
+    path20000 = "HybirdLouvainSA/HybirdLouvainSA/partitions/20000"
     path50000 = "HybirdLouvainSA/HybirdLouvainSA/partitions/50000"
 
-    Hybrid, Louvain, SA = load_files(path1000)
+    Hybrid10000, Louvain10000, SA10000 = load_files(path10000)
+    Hybrid20000, Louvain20000, SA20000 = load_files(path20000)
+    Hybrid50000, Louvain50000, SA50000 = load_files(path50000)
 
-    path1000answer = "graphGeneration/graphs/10000"
-    path10000answer = "graphGeneration/graphs/20000"
+    path10000answer = "graphGeneration/graphs/10000"
+    path20000answer = "graphGeneration/graphs/20000"
     path50000answer = "graphGeneration/graphs/50000"
-    Answers = get_location(path1000answer)
-    
 
-    Results_Hybrid = calculateNMI(Hybrid, Answers)
-    Results_Louvain = calculateNMI(Louvain, Answers)
+    Answers10000 = get_location(path10000answer)
+    Answers20000 = get_location(path20000answer)
+    Answers50000 = get_location(path50000answer)
 
-    writing_csv("Hybrid", Results_Hybrid)
-    writing_csv("Louvain", Results_Louvain)
-    
-    print(Results_Hybrid)
+    # extra test
+    Results_Hybrid10000 = calculateNMI(SA10000, Answers10000)
 
+    Results_Hybrid10000 = calculateNMI(Hybrid10000, Answers10000)
+    Results_Louvain10000 = calculateNMI(Louvain10000, Answers10000)
+    Results_Hybrid20000 = calculateNMI(Hybrid20000, Answers20000)
+    Results_Louvain20000 = calculateNMI(Louvain20000, Answers20000)
+    Results_Hybrid50000 = calculateNMI(Hybrid50000, Answers50000)
+    Results_Louvain50000 = calculateNMI(Louvain50000, Answers50000)
 
+    Resultstotal = [Results_Hybrid10000, Results_Louvain10000, Results_Hybrid20000, Results_Louvain20000, Results_Hybrid50000, Results_Louvain50000]
 
+    printing_results(Resultstotal)
+    writing_txt(Resultstotal)
 
 
 
